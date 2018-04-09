82ddab2881c776e74a59cbcb31a61264499b511f
freddajuste <33464231+freddajuste@users.noreply.github.com>
2018-04-09 16:32:48 -0700

    made a new repository to see if it would let me deploy onto github

---
+++ is219-p2 (1)/js/gallery.js
@@ -0,0 +1,120 @@
+// requestAnim shim layer by Paul Irish
+    window.requestAnimFrame = (function(){
+      return  window.requestAnimationFrame       || 
+              window.webkitRequestAnimationFrame || 
+              window.mozRequestAnimationFrame    || 
+              window.oRequestAnimationFrame      || 
+              window.msRequestAnimationFrame     || 
+              function(/* function */ callback, /* DOMElement */ element){
+                window.setTimeout(callback, 1000 / 60);
+              };
+    })();
+  
+
+// example code from mr doob : http://mrdoob.com/lab/javascript/requestanimationframe/
+
+animate();
+
+var mLastFrameTime = 0;
+var mWaitTime = 5000; //time in ms
+function animate() {
+    requestAnimFrame( animate );
+	var currentTime = new Date().getTime();
+	if (mLastFrameTime === 0) {
+		mLastFrameTime = currentTime;
+	}
+
+	if ((currentTime - mLastFrameTime) > mWaitTime) {
+		swapPhoto();
+		mLastFrameTime = currentTime;
+	}
+}
+
+/************* DO NOT TOUCH CODE ABOVE THIS LINE ***************/
+
+function swapPhoto() {
+	//Add code here to access the #slideShow element.
+	//Access the img element and replace its source
+	//with a new image from your images array which is loaded 
+	//from the JSON string
+	$('#slideShow').click(function(){
+		$('.img').text(galleryImage.next());
+	});
+	console.log('swap photo');
+}
+
+// Counter for the mImages array
+var mCurrentIndex = 0;
+
+// XMLHttpRequest variable
+var mRequest = new XMLHttpRequest();
+var mURL = "images.json";
+mRequest.onreadystatechange = function() {
+      if (mRequest.readyState == 4 && mRequest.status == 200) {
+          try {
+               
+               mJson = JSON.parse(mRequest.responseText);
+               
+               console.log(mJson);
+          } catch(err) {
+               console.log(err.message);
+} }
+};
+mRequest.open("GET",mURL, true);
+mRequest.send();
+
+// Array holding GalleryImage objects (see below).
+var mImages = [];
+
+// Holds the retrived JSON information
+var mJson;
+
+// URL for the JSON to load by default
+// Some options for you are: images.json, images.short.json; you will need to create your own extra.json later
+var mUrl = 'images.json';
+
+
+//You can optionally use the following function as your event callback for loading the source of Images from your json data (for HTMLImageObject).
+//@param A GalleryImage object. Use this method for an event handler for loading a gallery Image object (optional).
+function makeGalleryImageOnloadCallback(galleryImage) {
+	return function(e) {
+		galleryImage.img = e.target;
+		mImages.push(galleryImage);
+	}
+}
+
+$(document).ready( function() {
+	
+	// This initially hides the photos' metadata information
+	$('.details').eq(0).hide();
+	
+});
+
+window.addEventListener('load', function() {
+	
+	console.log('window loaded');
+
+}, false);
+
+function GalleryImage(location, description, date, img) {
+	//implement me as an object to hold the following data about an image:
+	//1. location where photo was taken
+	//2. description of photo
+	//3. the date when the photo was taken
+	//4. either a String (src URL) or an an HTMLImageObject (bitmap of the photo. https://developer.mozilla.org/en-US/docs/Web/API/HTMLImageElement)
+	this.location = new location;
+	this.description = new description;
+	this.date = new date;
+	this.img = new img;
+}
+$('img.moreIndicator').click(function(){
+	if (this.hasClass("rot90")){
+		this.add("rot270").remove("rot90");}
+	else if (this.hasClass("rot270"){
+		this.add("rot90").remove("rot270");}
+	else{}
+	$('div.details').fadeToggle("fast", function(){
+		$('img.moreIndicator').slideUp();
+	});
+	
+	});
